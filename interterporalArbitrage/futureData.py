d46adf987db258752ccff70eb666d78c627dba71
JasonGuoSkr <gl18253166772@163.com>
2020-05-11 16:54:00 +0800

    update

--- interterporalArbitrage/futureData.py
+++ interterporalArbitrage/futureData.py
@@ -4,13 +4,13 @@ import rqdatac as rq
 
 
 """
-期货tick数据加载
+期货分钟数据加载
 """
 
 
 def future_data_load(args, start_date, end_date):
     """
-    期货tick数据
+    期货分钟数据
     :param args: 合约list
     :param start_date: 开始日期
     :param end_date: 结束日期
@@ -18,50 +18,17 @@ def future_data_load(args, start_date, end_date):
     """
     if args:
         data_dict = dict()
-        date_list = rq.get_trading_dates(start_date=start_date, end_date=end_date)
 
         for contract_id in args:
-            data = pd.DataFrame()
-
-            for date_ind in date_list:
-                date_ind = date_ind.strftime('%Y%m%d')
-                try:
-                    daily_data = get_tick(contract_id, date_ind, date_ind)
-                except AttributeError:
-                    print(contract_id)
-                else:
-                    daily_data = data_resample(daily_data)
-                    data = pd.concat([data, daily_data], axis=0)
-
-            data_dict[contract_id] = data
+            try:
+                contract_data = rq.get_price(contract_id, start_date=start_date, end_date=end_date, frequency='tick', fields=None)
+            except AttributeError:
+                print(contract_id)
+                raise AttributeError
+            data_dict[contract_id] = contract_data
 
         return data_dict
 
-
-def data_resample(data, freq='500ms'):
-    """
-    数据填充补齐
-    :param data: 原数据
-    :param freq: 频率
-    :return:
-    """
-    data = data.resample(freq).ffill()
-    data = data.between_time('09:31:00', '14:56:00')
-
-    return data.between_time('13:00:00', '11:30:00')
-
-
-def get_tick(contract_id, start_date, end_date):
-    """
-    tick数据下载
-    :param contract_id:
-    :param start_date:
-    :param end_date:
-    :return:
-    """
-    df_tick = rq.get_price(contract_id, start_date=start_date, end_date=end_date, frequency='tick', fields=None)
-
-    return df_tick.between_time('09:25:00', '15:01:00')
 
 
 if __name__ == '__main__':
