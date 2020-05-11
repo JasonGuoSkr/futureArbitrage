e159c697254deb99d3a11d0a2e7a34dfb6d2e576
JasonGuoSkr <gl18253166772@163.com>
2020-05-11 18:17:44 +0800

    update

--- interterporalArbitrage/futureData.py
+++ interterporalArbitrage/futureData.py
@@ -11,34 +11,44 @@ import rqdatac as rq
 def future_data_load(args, start_date, end_date):
     """
     期货分钟数据
-    :param args: 合约list
+    :param args: 合约list or 合约str
     :param start_date: 开始日期
     :param end_date: 结束日期
     :return:
     """
-    if args:
+    if type(args) is str:
+        contract_id = args
+        data_dict = {contract_id: pd.DataFrame()}
+        try:
+            contract_data = rq.get_price(contract_id, start_date=start_date, end_date=end_date,
+                                         frequency='1m', fields=None)
+        except AttributeError:
+            print(contract_id)
+            raise AttributeError
+        data_dict[contract_id] = contract_data
+    else:
         data_dict = dict()
 
         for contract_id in args:
             try:
-                contract_data = rq.get_price(contract_id, start_date=start_date, end_date=end_date, frequency='tick', fields=None)
+                contract_data = rq.get_price(contract_id, start_date=start_date, end_date=end_date,
+                                             frequency='1m', fields=None)
             except AttributeError:
                 print(contract_id)
                 raise AttributeError
             data_dict[contract_id] = contract_data
 
-        return data_dict
+    return data_dict
 
 
 
 if __name__ == '__main__':
     rq.init("ricequant", "8ricequant8", ('10.29.135.119', 16010))
 
-    # trade_date = datetime.datetime.now().strftime('%Y%m%d')
     startDate = '20200302'
     endDate = '20200309'
 
-    contractList = ('IF2003', 'IH2003')
-    # contractList = 'IF2003'
+    # contractList = ('IF2003', 'IF2004')
+    contractList = 'IF2003'
 
     Data = future_data_load(contractList, start_date=startDate, end_date=endDate)
