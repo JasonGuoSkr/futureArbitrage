362825677828e2f3861600647df0853458a27c3c
JasonGuoSkr <gl18253166772@163.com>
2020-05-13 16:23:42 +0800

    update

--- calendarArbitrage/calendarStrategy.py
+++ calendarArbitrage/calendarStrategy.py
@@ -16,13 +16,20 @@ import rqdatac as rq
 if __name__ == '__main__':
     rq.init("ricequant", "8ricequant8", ('10.29.135.119', 16010))
 
+    # 文件路径
+    outputPath = "E:/中泰证券/策略/期货套利/日历效应/"
+    if not os.path.exists(outputPath):
+        os.makedirs(outputPath)
+
     # 参数
     dateLen = 10
     startBenchDate = '0420'
     endBenchDate = '0430'
-    contractList = ('000016.XSHG', '000300.XSHG','000905.XSHG', '000852.XSHG', '399006.XSHE')
+    contractList = ('000016.XSHG', '000300.XSHG', '000905.XSHG', '000852.XSHG', '399006.XSHE')
 
     # 计算
+    pctChange = pd.DataFrame(index=range(2011, 2021), columns=contractList)
+
     for year_id in range(2011, 2021):
         # year_id = 2011
         print(year_id)
@@ -41,3 +48,6 @@ if __name__ == '__main__':
         dataDf.columns = contractList
 
         pctChangeDf = dataDf / dataDf.iloc[0, :]
+        pctChange.loc[year_id] = pctChangeDf.iloc[-1, :] - 1
+
+    pctChange.to_csv(outputPath + endBenchDate + "指数收益.csv")
