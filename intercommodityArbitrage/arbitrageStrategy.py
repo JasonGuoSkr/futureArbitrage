0349f7aecee8c7ef9541d2b82d6ed6e6277db27f
JasonGuoSkr <gl18253166772@163.com>
2020-05-19 19:06:56 +0800

    update

--- intercommodityArbitrage/arbitrageStrategy.py
+++ intercommodityArbitrage/arbitrageStrategy.py
@@ -18,11 +18,19 @@ import rqdatac as rq
 """
 
 
-def trading_data(trade_date, contract_list):
+def trading_data(contract_list, start_date, end_date):
     # 数据加载
-    data_load = intercommodityArbitrage.futureData.future_data_load(contract_list, start_date=trade_date,
-                                                                    end_date=trade_date)
+    data_load = intercommodityArbitrage.futureData.future_data_load(contract_list, start_date=start_date,
+                                                                    end_date=end_date)
+    data_df = pd.DataFrame()
+    data_df['trading_date'] = data_load[contract_list[0]]['trading_date']
+    columns_list = ['last', 'a1', 'b1']
+    for contract_id in contract_list:
+        contract_data = data_load[contract_id][columns_list]
+        contract_data.columns = [contract_id + '_' + columns_name for columns_name in columns_list]
+        data_df = pd.concat([data_df, contract_data], axis=1)
 
+    return data_df
 
 
 if __name__ == '__main__':
@@ -34,8 +42,7 @@ if __name__ == '__main__':
     contractList = ('IF2005', 'IH2005')
 
     # 数据加载
-    futureData = intercommodityArbitrage.futureData.future_data_load(contractList, start_date=startDate,
-                                                                     end_date=endDate)
+    futureData = trading_data(contractList, start_date=startDate, end_date=endDate)
     spreadData = intercommodityArbitrage.spreadCompute.spread_compute(startDate, endDate, contractList)
 
     #
