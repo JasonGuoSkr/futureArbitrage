2e6f2559f16c684eb6a8ea3b82986b5b07256f3b
JasonGuoSkr <gl18253166772@163.com>
2020-06-28 13:50:57 +0800

    update

--- intercommodityArbitrage/futureData.py
+++ intercommodityArbitrage/futureData.py
@@ -24,14 +24,14 @@ def future_data_load(args, start_date, end_date):
             data = pd.DataFrame()
 
             for date_ind in date_list:
-                date_ind = date_ind.strftime('%Y%m%d')
+                # date_ind = date_ind.strftime('%Y%m%d')
                 try:
                     daily_data = get_tick(contract_id, date_ind, date_ind)
                 except AttributeError:
                     print(contract_id)
                 else:
-                    daily_data.drop_duplicates(keep='first', inplace=True)
-                    # daily_data = daily_data.loc[~daily_data.index.duplicated(keep='first')]
+                    # daily_data.drop_duplicates(keep='first', inplace=True)
+                    daily_data = daily_data.loc[~daily_data.index.duplicated(keep='first')]
                     daily_data = data_resample(daily_data)
                     data = pd.concat([data, daily_data], axis=0)
 
@@ -48,7 +48,7 @@ def data_resample(data, freq='500ms'):
     :return:
     """
     data = data.resample(freq).ffill()
-    data = data.between_time('09:30:30', '14:56:00')
+    data = data.between_time('09:30:00', '15:00:00')
 
     return data.between_time('13:00:00', '11:30:00')
 
@@ -63,17 +63,19 @@ def get_tick(contract_id, start_date, end_date):
     """
     df_tick = rq.get_price(contract_id, start_date=start_date, end_date=end_date, frequency='tick', fields=None)
 
-    return df_tick.between_time('09:25:00', '15:01:00')
+    return df_tick.between_time('09:25:00', '15:10:00')
 
 
 if __name__ == '__main__':
     rq.init("ricequant", "8ricequant8", ('10.29.135.119', 16010))
 
     # trade_date = datetime.datetime.now().strftime('%Y%m%d')
-    startDate = '20200302'
+    startDate = '20200304'
     endDate = '20200309'
 
     contractList = ('IF2003', 'IH2003')
     # contractList = 'IF2003'
 
     Data = future_data_load(contractList, start_date=startDate, end_date=endDate)
+    # initialTick = get_tick('IF2003', startDate, endDate)
+    # resampleTick = data_resample(initialTick, freq='500ms')
