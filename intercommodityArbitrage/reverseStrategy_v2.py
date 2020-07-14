e61566e5c257d356c5e9a36868853813024f8344
JasonGuoSkr <gl18253166772@163.com>
2020-07-14 16:13:18 +0800

    update

--- intercommodityArbitrage/reverseStrategy_v2.py
+++ intercommodityArbitrage/reverseStrategy_v2.py
@@ -9,7 +9,7 @@ import pandas as pd
 import plotly.graph_objs as go
 from plotly.subplots import make_subplots
 import intercommodityArbitrage.futureData
-import intercommodityArbitrage.spreadCompute
+import intercommodityArbitrage.spreadAnalysis
 import rqdatac as rq
 
 
@@ -20,49 +20,40 @@ import rqdatac as rq
 修改内容：
     增加**************
     
-    
-    
 """
 
 
-def trading_data(contract_list, start_date, end_date):
-    """
-    获取期货合约的交易数据，包括日期、最新价、买一价、卖一价等(['trading_date', 'last', 'a1', 'b1'])
-    :param contract_list: 合约列表
-    :param start_date: 开始日期
-    :param end_date: 结束日期
-    :return: 合并数据
-    """
-    data_load = intercommodityArbitrage.futureData.future_data_load(contract_list, start_date=start_date,
-                                                                    end_date=end_date)
+def trading_data(underlying_list, start_date, end_date):
+    date_list = rq.get_trading_dates(start_date=start_date, end_date=end_date)
+    price_data = pd.DataFrame()
 
-    data_df = pd.DataFrame()
-    data_df['trading_date'] = data_load[contract_list[0]]['trading_date']
-    columns_list = ['last', 'a1', 'b1']
-    for contract_id in contract_list:
-        contract_data = data_load[contract_id][columns_list]
-        contract_data.columns = [contract_id + '_' + columns_name for columns_name in columns_list]
-        data_df = pd.concat([data_df, contract_data], axis=1)
+    for date_ind in date_list:
+        print(date_ind)
+        contract_list = []
+        for underlying in underlying_list:
+            contract = rq.futures.get_dominant(underlying, start_date=date_ind, end_date=date_ind, rule=0)
+            contract_list.append(contract[date_ind])
 
-    return data_df
+        data_load = intercommodityArbitrage.futureData.future_data_load(contract_list, start_date=date_ind,
+                                                                        end_date=date_ind)
+
+        data_df = pd.DataFrame()
+        data_df['trading_date'] = data_load[contract_list[0]]['trading_date']
+        columns_list = ['last', 'a1', 'b1']
+        for contract_id in contract_list:
+            contract_data = data_load[contract_id][columns_list]
+            contract_data.columns = [contract_id[:2] + '_' + columns_name for columns_name in columns_list]
+            data_df = pd.concat([data_df, contract_data], axis=1)
+
+        price_data = pd.concat([price_data, data_df], axis=0)
+
+    return price_data
 
 
-def strategy(contract_list, start_date, end_date, diff=0.0015, stop=-0.001, close=0.001, open_len=1200, close_len=7200):
-    """
-    策略回测，短时间(dateLen)内价差(spread_pct)大幅波动，反向建仓，按一定比例止盈或止损/持仓超过一定时间平仓/日内强制平仓
-    :param contract_list: 合约列表
-    :param start_date: 开始日期
-    :param end_date: 结束日期
-    :param diff: 波动阈值
-    :param stop: 止损阈值
-    :param close: 止盈阈值
-    :param open_len: 入场时间长度
-    :param close_len: 出场时间长度
-    :return: 交易细节
-    """
+def strategy(underlying_list, start_date, end_date, diff=0.0015, stop=-0.001, close=0.001, open_len=1200, close_len=7200):
     # 数据加载
-    future_data = trading_data(contract_list, start_date=start_date, end_date=end_date)
-    spread_data = intercommodityArbitrage.spreadCompute.spread_compute(start_date, end_date, contract_list)
+    future_data = trading_data(underlying_list, start_date=start_date, end_date=end_date)
+    spread_data = intercommodityArbitrage.spreadAnalysis.spread_analysis(underlying_list, start_date, end_date)
 
     # 逐tick回测，获取交易信号
     trade_details = pd.DataFrame(columns=['openTime', 'closeTime', 'tradeDirection',
@@ -180,8 +171,8 @@ def strategy(contract_list, start_date, end_date, diff=0.0015, stop=-0.001, clos
         # order = trade_details.index[0]
         open_time = trade_details.loc[order, 'openTime']
         close_time = trade_details.loc[order, 'closeTime']
-        contract_0 = contract_list[0]
-        contract_1 = contract_list[1]
+        contract_0 = underlying_list[0]
+        contract_1 = underlying_list[1]
         if trade_details.loc[order, 'tradeDirection'] == 1:
             long_leg = (future_data.loc[close_time, contract_0 + '_b1'] -
                         future_data.loc[open_time, contract_0 + '_a1']) / \
